516ed9672b75fc8e5ecdc9e0da868bb2be15c875
Shuyibe48 <gthrough48@gmail.com>
2023-03-07 11:07:56 +0600

    rest

--- script/index.js
+++ script/index.js
@@ -1,1 +1,11 @@
 console.log('clicked')
+
+setTimeout(function(){
+    console.log('im done!')
+}, 2000)
+
+// let time = 0
+// setInterval(function(){
+//     time++
+//     console.log(time)
+// }, 1000)
