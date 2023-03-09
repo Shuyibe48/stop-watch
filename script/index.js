05c447795ad09b2b6492b2757a2a4683ba833cf2
Shuyibe48 <gthrough48@gmail.com>
2023-03-09 22:59:31 +0600

    work done without styles

--- script/index.js
+++ script/index.js
@@ -1,11 +1,50 @@
-console.log('clicked')
+let [hours, minutes, seconds] = [0, 0, 0]
+const displayTime = document.getElementById('display-time')
+let timer = null
 
-setTimeout(function(){
-    console.log('im done!')
-}, 2000)
+const stopWatch = () => {
+    seconds++
+    if(seconds === 60){
+        seconds = 0
+        minutes++
+        if(minutes === 60){
+            minutes = 0
+            hours++
+        }
+    }
 
-// let time = 0
-// setInterval(function(){
-//     time++
-//     console.log(time)
-// }, 1000)
+    let h = hours < 10 ? '0' + hours : hours
+    let m = minutes < 10 ? '0' + minutes : minutes
+    let s = seconds < 10 ? '0' + seconds : seconds
+
+    displayTime.innerText = h + ':' + m + ':' + s
+}
+
+const startWatch = () => {
+    if(timer !== null){
+        clearInterval(timer)
+    }
+    timer = setInterval(stopWatch, 1000)
+}
+
+const poseWatch = () => {
+    clearInterval(timer)
+}
+
+const reSetWatch = () => {
+    clearInterval(timer)
+    hours = 0
+    minutes = 0
+    seconds = 0
+    displayTime.innerHTML = '00:00:00'
+}
+
+document.getElementById('start').addEventListener('click', function(){
+    startWatch()
+})
+document.getElementById('stop').addEventListener('click', function(){
+    poseWatch()
+})
+document.getElementById('reset').addEventListener('click', function(){
+    reSetWatch()
+})
